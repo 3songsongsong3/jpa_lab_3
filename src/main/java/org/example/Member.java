581a3c6550b98487d1e0b73d789a14c0607ccb6f
tlahsh12 <tlahsh12@naver.com>
2023-09-03 19:03:04 +0900

    기본키 매핑 추가

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -8,12 +8,25 @@ import java.util.Date;
 // DDL을 자동 생성할 때만 사용되고, JPA의 실행 로직에는 영향을 주지 않는다.
 // -> 그래도 개발자가 엔티티만 보고도 다양한 제약조건을 파악할 수 있는 장점이 있다.
 @Entity
+@SequenceGenerator(name = "TEST_SEQ_GENERATOR",
+                    sequenceName = "TEST_SEQ",
+                    initialValue = 1, allocationSize = 1 // allocationSize의 default는 50이다.
+)
 @Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
         name = "NAME_AGE_UNIQUE",
         columnNames = {"NAME", "AGE"} )})
 public class Member {
 
     @Id
+    // 1. Identity 전략으로 기본키 생성을 데이터베이스에 위임함
+    // 데이터베이스에 엔티티를 저장해서 식별자 값을 획득, 영속성 컨텍스트에 저장
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 2. 데이터베이스 시퀀스 생성용 테이블에서 식별자 값을 획득한 후 영속성 컨텍스트에 저장
+    // @GeneratedValue(strategy = GenerationType.TABLE, generator = "TEST_SEQ_GENERATOR")
+    // 3. 데이터베이스 방언에 따라 IDENTITY, SEQUENCE, TABLE 전략 중 하나를 자동으로 선택
+    // @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "TEST_SEQ_GENERATOR") // 시퀀스 전략
     @Column(name = "ID")
     private String id;
 
